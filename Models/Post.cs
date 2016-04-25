4ed243fa105248be678d61a939e042689b988c1f
Simon Lawton <simonctemple@live.com lawton.a.browning@gmail.com>
2016-04-25 14:35:08 -0700

    add all basic functionality

---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackClone.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        public int id { get; set; }
+        public string Content { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
